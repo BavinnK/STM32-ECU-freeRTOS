98875cc6bc06b7d93e1dd54f1b445551610a7f67
Bavin <bavinbav5@gmail.com>
2025-12-16 20:16:44 +0300

    added required files for the FreeRTOS into a new folder which is thirdparty

--- Core/Src/MyAdc.c
+++ Core/Src/MyAdc.c
@@ -2,42 +2,50 @@
 #include "stm32f446xx.h"
 
 void adc_init(void) {
-	//PA0 for the LM35 ADC1 IN0
-	//PA1 for the pot  ADC1 IN1
-	RCC->AHB1ENR |= (1 << 0);	//ENABLE GPIOA
-	RCC->APB2ENR |= (1 << 8);   //ENABLE ADC1
+	// PA0 for the LM35 ADC1 IN0
+	// PA1 for the pot  ADC1 IN1
+
+	RCC->AHB1ENR |= (1 << 0);	// ENABLE GPIOA
+	RCC->APB2ENR |= (1 << 8);   // ENABLE ADC1
+
+	// Set PA0 and PA1 to analog mode
 	GPIOA->MODER &= ~(0b1111);
-	GPIOA->MODER |= (0b11) | (0b11 << 2);		//PA0 and PA1
+	GPIOA->MODER |= (0b11) | (0b11 << 2);	// PA0 and PA1
 
-	ADC1->CR2 |= (0b1);
+	// VERY IMPORTANT for LM35: long sample time
+	// Channel 0 (PA0)
+	ADC1->SMPR2 |= (0b111 << 0);	// 480 cycles for CH0
+	// Channel 1 (PA1)
+	ADC1->SMPR2 |= (0b111 << 3);	// 480 cycles for CH1
 
+	// Enable ADC
+	ADC1->CR2 |= (1 << 0);
+
+	// Small delay to let ADC stabilize
+	for (volatile int i = 0; i < 10000; i++);
+
+	// Calibrate ADC (IMPORTANT to remove offset error)
+	ADC1->CR2 |= (1 << 3);
+	while (ADC1->CR2 & (1 << 3));	// wait until calibration finishes
 }
 
 uint16_t adc_read(uint8_t channel) {
-	ADC1->SQR3 &= ~((0b1111111111));//clearing SQ1 AND SQ2,  SQ1 is gived for PA0 ,and SQ2 for PA1
-	//ADC1->SQR3 |= (1 << 5) | (0 << 0); //channel 0 == SQ1 and channel 1== SQ2
 
-	if (channel == 0) {
-		ADC1->SQR3 &= ~(0b1111);
-		ADC1->SQR3 |= 0;
-		ADC1->CR2 &= ~(1 << 30);	//reset state then set
-		ADC1->CR2 |= (1 << 30);
+	// Clear SQ1 (we use only one conversion)
+	ADC1->SQR3 &= ~(0b11111);
 
-		while (!(ADC1->SR & (1 << 1)));	//we wait until the coversion is complete then return the value
+	// Select channel:
+	// channel 0 -> PA0 (LM35)
+	// channel 1 -> PA1 (pot)
+	ADC1->SQR3 |= channel;
 
-		return ADC1->DR;
+	// Start conversion by software
+	ADC1->CR2 &= ~(1 << 30);	// reset state
+	ADC1->CR2 |= (1 << 30);		// start conversion
 
-	} else if (channel == 1) {
-		ADC1->SQR3 &= ~(0b1111 << 5);
-		ADC1->SQR3 |= (1 << 5);
-		ADC1->CR2 &= ~(1 << 30);	//reset state then set
-		ADC1->CR2 |= (1 << 30);
+	// Wait until conversion is complete
+	while (!(ADC1->SR & (1 << 1)));
 
-		while (!(ADC1->SR & (1 << 1)));	//we wait until the coversion is complete then return the value
-
-		return ADC1->DR;
-	}
-
-	return 0;//if the channel number was wrong the user has to choose 1 or 0 to read
-
+	// Reading DR clears EOC flag automatically
+	return ADC1->DR;
 }
