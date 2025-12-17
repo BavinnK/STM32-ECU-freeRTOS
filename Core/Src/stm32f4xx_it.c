f66132187261648922edd80b80f63a295fccd307
Bavin <bavinbav5@gmail.com>
2025-12-17 22:54:04 +0300

    we have pulse baby the RTOS is alive, and now we will move every driver we created and move them into thier own task and priority

--- Core/Src/stm32f4xx_it.c
+++ Core/Src/stm32f4xx_it.c
@@ -86,11 +86,12 @@ void HardFault_Handler(void)
   /* USER CODE BEGIN HardFault_IRQn 0 */
 
   /* USER CODE END HardFault_IRQn 0 */
-  while (1)
-  {
-    /* USER CODE BEGIN W1_HardFault_IRQn 0 */
-    /* USER CODE END W1_HardFault_IRQn 0 */
-  }
+
+	  while (1)
+	  {
+	    // If you hit a hard fault, you will get stuck here.
+	    // Put a breakpoint on this line in the debugger.
+	  }
 }
 
 /**
