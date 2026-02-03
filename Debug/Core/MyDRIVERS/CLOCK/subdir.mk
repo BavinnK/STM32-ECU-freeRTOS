d3b712751bb8fc677ba6249f7fcb871567158b26
Bavin <bavinbav5@gmail.com>
2026-02-03 20:09:59 +0300

    added my clock driver that i wrote to this project so we can get maximum speed

--- Debug/Core/MyDRIVERS/CLOCK/subdir.mk
+++ Debug/Core/MyDRIVERS/CLOCK/subdir.mk
@@ -5,23 +5,23 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
-../Core/MyDRIVERS/GPIO/gpiox.c 
+../Core/MyDRIVERS/CLOCK/clock.c 
 
 OBJS += \
-./Core/MyDRIVERS/GPIO/gpiox.o 
+./Core/MyDRIVERS/CLOCK/clock.o 
 
 C_DEPS += \
-./Core/MyDRIVERS/GPIO/gpiox.d 
+./Core/MyDRIVERS/CLOCK/clock.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
-Core/MyDRIVERS/GPIO/%.o Core/MyDRIVERS/GPIO/%.su Core/MyDRIVERS/GPIO/%.cyclo: ../Core/MyDRIVERS/GPIO/%.c Core/MyDRIVERS/GPIO/subdir.mk
+Core/MyDRIVERS/CLOCK/%.o Core/MyDRIVERS/CLOCK/%.su Core/MyDRIVERS/CLOCK/%.cyclo: ../Core/MyDRIVERS/CLOCK/%.c Core/MyDRIVERS/CLOCK/subdir.mk
 	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F446xx -c -I"C:/Users/pc/Desktop/STM_PROJECTS/V2.4_ECU_STM32_PROJECT/Core/Inc/ILI9341" -I"C:/Users/pc/Desktop/STM_PROJECTS/V2.4_ECU_STM32_PROJECT/Core/MyDRIVERS/CLOCK" -I"C:/Users/pc/Desktop/STM_PROJECTS/V2.4_ECU_STM32_PROJECT/Core/MyDRIVERS/USART" -I"C:/Users/pc/Desktop/STM_PROJECTS/V2.4_ECU_STM32_PROJECT/Core/MyDRIVERS/GPIO" -I"C:/Users/pc/Desktop/STM_PROJECTS/V2.4_ECU_STM32_PROJECT/Core/MyDRIVERS/DMA" -I"C:/Users/pc/Desktop/STM_PROJECTS/V2.4_ECU_STM32_PROJECT/Core/MyDRIVERS/ADC" -I"C:/Users/pc/Desktop/STM_PROJECTS/V2.4_ECU_STM32_PROJECT/Core/Inc" -I"C:/Users/pc/Desktop/STM_PROJECTS/V2.4_ECU_STM32_PROJECT/Core/ThirdParty/FreeRTOS/include" -I"C:/Users/pc/Desktop/STM_PROJECTS/V2.4_ECU_STM32_PROJECT/Core/ThirdParty/FreeRTOS/portable" -I../Drivers/STM32F4xx_HAL_Driver/Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../Drivers/CMSIS/Include -I../Core/Inc -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
 
-clean: clean-Core-2f-MyDRIVERS-2f-GPIO
+clean: clean-Core-2f-MyDRIVERS-2f-CLOCK
 
-clean-Core-2f-MyDRIVERS-2f-GPIO:
-	-$(RM) ./Core/MyDRIVERS/GPIO/gpiox.cyclo ./Core/MyDRIVERS/GPIO/gpiox.d ./Core/MyDRIVERS/GPIO/gpiox.o ./Core/MyDRIVERS/GPIO/gpiox.su
+clean-Core-2f-MyDRIVERS-2f-CLOCK:
+	-$(RM) ./Core/MyDRIVERS/CLOCK/clock.cyclo ./Core/MyDRIVERS/CLOCK/clock.d ./Core/MyDRIVERS/CLOCK/clock.o ./Core/MyDRIVERS/CLOCK/clock.su
 
-.PHONY: clean-Core-2f-MyDRIVERS-2f-GPIO
+.PHONY: clean-Core-2f-MyDRIVERS-2f-CLOCK
 
--- Debug/sources.mk
+++ Debug/sources.mk
@@ -23,6 +23,7 @@ C_DEPS :=
 # Every subdirectory with source files must be described here
 SUBDIRS := \
 Core/MyDRIVERS/ADC \
+Core/MyDRIVERS/CLOCK \
 Core/MyDRIVERS/DMA \
 Core/MyDRIVERS/GPIO \
 Core/MyDRIVERS/USART \
